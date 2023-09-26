b749c0f19616b6666c47d535dd23068188ec8e78
enviesm123 <enviesm@naver.com>
2023-09-26 13:53:45 +0900

    for문 시연

--- src/ch05_controll/LoopFor.java
+++ src/ch05_controll/LoopFor.java
@@ -86,5 +86,147 @@ public class LoopFor {
         }
         System.out.println(jjakSum);
 
+
+        sum = 0;
+        // 실행결과는 같은데 for문의 실행 횟수는 더 적다.
+        // (프로그램 실행 속도 증가 = 성능 증가)
+        for(int i = 0; i <= 40; i +=2){
+            // i = 0, i = 2, i = 4
+            sum += i;
+        }
+        System.out.println(sum);
+
+        // 0부터 2까지 0.2마다 출력
+        // 소수인 double, float 타입의 연산은 정확하지 않으므로 비추천
+        for(double i = 0; i <= 2; i += 0.2){
+            System.out.println(i);
+        }
+
+        // 0부터 2까지 0.2마다 출력
+        for(int i = 0; i <= 20;i+= 2){ // 0, 2, 4, 6, 8, ...,20
+            System.out.println((double) i / 10); // 0, 0.2, 0.4, ...,2
+        }
+
+        System.out.println("\n==================\n");
+
+        // 구구단 2단
+        // 2 x 1 = 2
+        // 2 x 2 = 4
+        // 2 x 9 = 18
+
+        for(int i = 1; i <= 9; i++){
+            System.out.println("2 X "+ i + " = " + (2 * i));
+        }
+
+        System.out.println("\n==================\n");
+
+        System.out.println("*");
+        System.out.println("**");
+        System.out.println("***");
+        System.out.println("*****");
+        System.out.println("******");
+
+        // 위의 코드를 for문을 이용하여 같은 결과가 나오도록 만들기
+        String star = "";
+        for(int i = 0; i < 5; i++){
+           star = star + "*"; // "" + "*" = "*", "*" + "*" = "**"
+            System.out.println(star);
+        }
+
+        System.out.println("\n==================\n");
+
+        // 나머지 연산자 (%) 의 사용예시
+        // for문 내에서 순환하는 숫자에 대해 사용하면좋다
+
+        // 국수공장에서 면을 20cm 뽑고 있다.
+        for(int i = 0; i < 20; i++){
+            System.out.println("||||||||");
+
+            // i가 4, 9, 14, ... 일때 절단 시키기
+            if(i == 4 || i == 9 || i == 14){
+                System.out.println("--------");
+            }
+
+            // 4, 9, 14, 19, 24, ... 는
+            // 전부 5로 나누었을때 나머지가 4인 숫자들
+            if(i % 5 == 4){
+                System.out.println("--------");
+            }
+
+        }
+
+        System.out.println("\n==================\n");
+
+        // 라면 공장에서 면을 30cm 뽑고 있는데
+        // 6cm 마다 잘라주어야 한다. 5 11 17 23 29
+        //  ////////
+        //  \\\\\\\\
+        // ////////
+        // \\\\\\\\
+
+        for(int i = 0; i < 30; i++){
+
+            if(i % 2 == 0){ // 홀짝 판별을 위함
+                System.out.println("((((((((");
+            }else{
+                System.out.println("))))))))");
+            }
+
+            // i가 5,11,17,23,... 일때 자른다.
+            // 6으로 나누었을때 나머지가 5인 숫자.
+            // 조건을 체크하는 목적이 다른 if문은 서로 분리해야 한다
+            if(i % 6 == 5){
+                System.out.println("--------");
+            }
+
+        }
+
+        System.out.println("\n==================\n");
+
+        // 10부터 1까지 출력
+        // 거꾸로 for문
+        for(int i = 10; i > 0; i--){
+            System.out.println(i);
+        }
+
+        System.out.println("\n==================\n");
+
+        // for문으로 String을 가지고 놀기
+
+        // 숫자형 문자열의 각 자리수를 더한 결과 얻기
+        // submit02에서 했던걸 for문을 이용해서 해결
+
+        String example = "37845";
+
+        // 문자열의 길이만큼 반복
+        int sumExam =0;
+        for(int i = 0; i < example.length(); i++){
+            String example1 = example.substring(i, i+1); // "3"
+            sumExam += Integer.parseInt(example1);
+        }
+        System.out.println(sumExam);
+
+        System.out.println("\n==================\n");
+
+        // 슈의 갯수는?
+        String syusyu = "슈슈슈슈슈슛슈슈슈슈슈슈슈슈ㅠㅠㅅㅅ슈슈슈슈"; //17
+
+        // syusyu 에서 "슈"가 몇글자 들어가 있는지 세어보기
+        // (눈으로 세어보기 17개)
+        // 개수 체크
+        int syuCnt = 0;
+        for(int i = 0; i < syusyu.length(); i++){
+            // 문자 한글자씩 떼기
+            String syusyu1 = syusyu.substring(i, i+1);
+            // 슈가 있는글자 체크
+            if(syusyu1.contains("슈")){
+                // 개수 세기
+                syuCnt++;
+            }
+        }
+        System.out.println("슈의 갯수: " + syuCnt);
+
+
+
     }
 }
---
+++ src/taegon/submit03/Submit03.java
@@ -0,0 +1,83 @@
+package taegon.submit03;
+
+public class Submit03 {
+
+    public static void main(String[] args) {
+//        팩토리얼 다들 기억하시나요?
+//                수학기호로 4! 는 4 팩토리얼을 의미하며,
+//        그 값은 4x3x2x1 = 24 입니다.
+//                for문을 사용해서 10팩토리얼의 값을 출력해주세요.
+
+        int gop = 1;
+        // 10! 팩토리얼 구하기  10 * 9 * 8 * 7 * 6 * 5 * 4 * 3 * 2 * 1
+        for(int i = 10; i > 1; i--){
+            gop *= i; // gop = gop * i
+        }
+        System.out.println(gop);
+
+        System.out.println("\n========================\n");
+
+        gop = 1;
+        long gop1 = 1L;
+        for(int g = 15; g > 1; g--){
+          gop1 = gop1 * g; // gop = gop * i
+        }
+        System.out.println(gop1);
+//        월리를 찾아라!!
+//                아래의 문자열 변수 findWally 중에 월리가 몇개 들어있는지 콘솔에 출력해주세요.
+                String findWally = "윌리울리일리울리울리일리월리일리윌리월리울리일리일리월리일리윌리일리윌리일" +
+                        "리월리월리윌리울리윌리울리일리울리울리윌리일리";
+//        Hint
+//        수업시간에 해보았던 "슈"는 문자열을 한글자씩 잘라서 "슈"가 맞는지 비교하였다면,
+//                이번에는 두글자씩 잘라서 "월리"가 맞는지 비교하시면 됩니다.
+//
+//
+//        결과: 5
+        int cntWally = 0;
+        for(int i = 0; i < findWally.length()-1; i++){
+            // 두글자씩 자르기 (0,2)(2,4)(4,6)
+            System.out.println(findWally.substring(i, i+2));
+            //월리가 있는지 체크
+            if(findWally.substring(i, i+2).equals("월리")){
+                // 월리가 있으면 숫자올리기
+                cntWally++;
+            }
+        }
+        System.out.println(cntWally);
+
+
+//        Q. 03 거꾸로 트리를 5층 만들어주세요.
+
+//        for문을 5번 반복하면서 stars를 아래와 같이 나오도록 잘라서 출력해보세요
+
+//        Hint
+//        for문 내에서 .substring() 사용
+        String stars = "*****"; //
+        // 5, 4, 3, 2, 1 나오게 하기
+        for(int i = 5; i+5 > stars.length(); i--){
+
+            // 별을 한개씩 줄여본다
+            System.out.println(stars.substring(0, i));
+
+//            System.out.println(stars.substring(0, i));
+
+            }
+
+//        Q. 04
+//        로꾸꺼 로꾸꺼
+//        사용자에게 문자열을 입력받으시고, 입력받은 문자열의 순서를 뒤집어서 출력해보세요.
+//        Hint
+//        for문 바깥에 문자열 변수 선언
+//        for문 내부가 반복될때마다 입력받은 문자열을 한글자씩 잘라 문자열 변수에 더한다.
+
+
+
+
+
+
+
+
+
+
+    }
+}
