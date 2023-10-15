a1a02134fb20f4784ac39ee66adcbe91b133a774
enviesm123 <enviesm@naver.com>
2023-10-16 08:47:48 +0900

    수업내용 낚시 fish과

--- src/taegon/submit09/q02/Fish.java
+++ src/taegon/submit09/q02/Fish.java
@@ -1,26 +1,30 @@
 package taegon.submit09.q02;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class Fish {
     String name;
     int price;
-
+    // toString 오버라이드 필수 그래야 출력된다
     @Override
     public String toString() {
         return
                 name +
-                ", 가치: " + price;
+                        ", 가격: " + price;
     }
 
+    // 빈생성자 생성 그래야 빈 클래스를 만들수잇음
     public Fish() {
-    }
 
+    }
+    // 생성할때 fish(String name(스트링타입의 변수명 name, int 타입의 변수명 price)로 쓰면 바로 추가가 가능
+    // fish.add(new Fish("장어", 100000));
     public Fish(String name, int price) {
         this.name = name;
         this.price = price;
     }
-
+    // getting setting 세트 모르면 걍 추가하셈 불러오고 변경할수있게 해준다
     public String getName() {
         return name;
     }
@@ -37,39 +41,68 @@ public class Fish {
         this.price = price;
     }
 
-    public ArrayList<Fish> fishing(){
-        ArrayList<Fish> fish = new ArrayList<>();
-        boolean isRun = true;
-
-
-        while(isRun){
-            int randInt = (int) (Math.random() * 6) + 1;
-            if (randInt == 1) {
-                fish.add(new Fish("고등어", 5000));
-                System.out.println("고등어를 잡았다!!");
-            } else if (randInt == 2) {
-                fish.add(new Fish("광어", 25000));
-                System.out.println("광어를 잡았다!!");
-            } else if (randInt == 3) {
-                fish.add(new Fish("우럭", 27000));
-                System.out.println("우럭을 잡았다!!");
-            } else if (randInt == 4) {
-                fish.add(new Fish("참돔", 30000));
-                System.out.println("참돔을 잡았다!!");
-            } else if (randInt == 5) {
-                fish.add(new Fish("돌돔", 35000));
-                System.out.println("돌돔을 잡았다!!");
-            } else if (randInt == 6) {
-                fish.add(new Fish("능성어", 70000));
-                System.out.println("능성어를 잡았다!!");
-            } else if (randInt == 7) {
-                fish.add(new Fish("다금바리", 120000));
-                System.out.println("다금바를 잡았다!!");
-            }
-            isRun = false;
+    // 1번 낚시를 하게 해주는 메서드
+    public ArrayList<Fish> fishing() {
+        ArrayList<Fish> oneFishing = new ArrayList<>();
+        // 경우의 수가 0부터 6까지 0 1 2 3 4 5 6 7가지
+        int randInt = (int) (Math.random() * 7);
+        // 랜덤 주사위를 굴려서 0이 나오면 고등어가 잡힘
+        // 랜덤 주사위를 굴려서 1이 나오면 광어가 잡힘
+        // 랜덤 주사위를 굴려서 2이 나오면 우럭이 잡힘
+        // 랜덤 주사위를 굴려서 3이 나오면 참돔이 잡힘
+        // 단 한번만 낚시 하므로 7개 중에 하나만 oneFishing에 추가된다
+        if (randInt == 0) {
+            oneFishing.add(new Fish("고등어", 5000));
+            System.out.println("고등어를 잡았다!!");
+        } else if (randInt == 1) {
+            oneFishing.add(new Fish("광어", 25000));
+            System.out.println("광어를 잡았다!!");
+        } else if (randInt == 2) {
+            oneFishing.add(new Fish("우럭", 27000));
+            System.out.println("우럭을 잡았다!!");
+        } else if (randInt == 3) {
+            oneFishing.add(new Fish("참돔", 30000));
+            System.out.println("참돔을 잡았다!!");
+        } else if (randInt == 4) {
+            oneFishing.add(new Fish("돌돔", 35000));
+            System.out.println("돌돔을 잡았다!!");
+        } else if (randInt == 5) {
+            oneFishing.add(new Fish("능성어", 70000));
+            System.out.println("능성어를 잡았다!!");
+        } else if (randInt == 6) {
+            oneFishing.add(new Fish("다금바리", 120000));
+            System.out.println("다금바리를 잡았다!!");
         }
 
-            return fish;
+        return oneFishing;
+    }
 
+    // 1번 낚시를 하게 해주는 메서드
+    public ArrayList<Fish> fishing2() {
+        ArrayList<Fish> oneFishing = new ArrayList<>();
+        // 경우의 수가 0부터 6까지 0 1 2 3 4 5 6 7가지
+        int randInt = (int) (Math.random() * 3);
+        // 랜덤 주사위를 굴려서 0이 나오면 고등어가 잡힘
+        // 랜덤 주사위를 굴려서 1이 나오면 광어가 잡힘
+        // 랜덤 주사위를 굴려서 2이 나오면 우럭이 잡힘
+        // 랜덤 주사위를 굴려서 3이 나오면 참돔이 잡힘
+        // 단 한번만 낚시 하므로 7개 중에 하나만 oneFishing에 추가된다
+
+        String[] fishs = {"고등어","광어","우럭","참돔","돌돔","능성어","다금바리"};
+        HashMap<String, Integer> fishPrice = new HashMap();
+        fishPrice.put("고등어", 5000);
+        fishPrice.put("광어", 25000);
+        fishPrice.put("우럭", 27000);
+        fishPrice.put("참돔", 30000);
+        fishPrice.put("돌돔", 35000);
+        fishPrice.put("능성어", 70000);
+        fishPrice.put("다금바리", 120000);
+        String fishName = fishs[randInt];
+        int fp =(int) fishPrice.get(fishs[randInt]);
+
+        oneFishing.add(new Fish(fishName,fp));
+        System.out.println(fishName +"를 잡았다!");
+
+        return oneFishing;
     }
 }
--- src/taegon/submit09/q02/FishMain.java
+++ src/taegon/submit09/q02/FishMain.java
@@ -1,52 +1,121 @@
 package taegon.submit09.q02;
-
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class FishMain {
     public static void main(String[] args) {
 
-//                Q. 02
-//        물반 고기반 낚시대를 던지면 물고기가 잡히는 낚시 게임을 만들어봅시다.
-//                물고기는 이름과 가격 속성을 가집니다.
-//        아래의 코드와 실행 예시를 참고하여, 물고기 객체에 대한 Fish 클래스를 생성해주세요.
-//                FishMain 클래스도 함께 만들어 아래의 코드를 옮겨적고 while문 내부를 완성시켜주세요.
+//      Q. 02
+//물반 고기반 낚시대를 던지면 물고기가 잡히는 낚시 게임을 만들어봅시다.
+//      물고기는 이름과 가격 속성을 가집니다.
+//아래의 코드와 실행 예시를 참고하여, 물고기 객체에 대한 Fish 클래스를 생성해주세요.
+//      FishMain 클래스도 함께 만들어 아래의 코드를 옮겨적고 while문 내부를 완성시켜주세요.
 //
-//         바다속 물고기 목록
-        ArrayList<Fish> fishList = new ArrayList<>();
+//바다속 물고기 목록
+//		ArrayList<Fish> fishList = new ArrayList<>();
+//		Fish oneFishing = new Fish(); // 이걸 못해서 몇시간 멍때렸다 메서드 불러오기 선언을 해서 불러와야된다 스캐너처럼 ㅠㅠ
+//
+//		fishList.add(new Fish("고등어", 5000));
+//		fishList.add(new Fish("광어", 25000));
+//		fishList.add(new Fish("우럭", 27000));
+//		fishList.add(new Fish("참돔", 30000));
+//		fishList.add(new Fish("돌돔", 35000));
+//		fishList.add(new Fish("능성어", 70000));
+//		fishList.add(new Fish("다금바리", 120000));
+//
+//		Scanner scan = new Scanner(System.in);
+//
+////잡은 물고기를 담을 가방
+//		ArrayList<Fish> fishBag = new ArrayList<>();
+//
+//		while (true) {
+//			System.out.println("행동을 선택해주세요.");
+//			System.out.println("1. 낚시하기 | 2. 잡은 물고기 목록 | 3. 떠나기");
+//			System.out.print(">>> ");
+//
+//			int select = Integer.parseInt(scan.nextLine());
+//			int sum = 0;
+//			// 낚시하기를 누르면 1번 낚시(0~7까지 랜덤으로 나온숫자에 해당하는 물고기)한 물고기 출력까지
+//			if (select == 1) {
+//				// 낚시한 물고기 리스트를 새로운 ArrayList에 담는다
+//				fishList = oneFishing.fishing();
+//				// 낚시해서 저장해놓은 모든 물고기 요소를 fishBag에 추가한다
+//				for (int i = 0; i < fishList.size(); i++) {
+//					fishBag.add(fishList.get(i));
+//				}
+//
+//				// TODO 낚시하기
+//			} else if (select == 2) {
+//				// TODO 잡은 물고기 목록 보기
+//				// fishBag에 담아놓은 모든 물고기를 출력한다
+//				System.out.println("내 가방 ==================");
+//				for (int i = 0; i < fishBag.size(); i++) {
+//
+//					System.out.println(fishBag.get(i));
+//					// fishBag에 잡힌 물고기 값을 다 합친다
+//					sum += (fishBag.get(i).getPrice());
+//				}
+//				System.out.println("총 " + sum + "원 " + "=================");
+//			} else if (select == 3) {
+//				// TODO 종료
+//				System.out.println("가지마세요ㅠㅠ");
+//				break;
+//			}
+//
+//		}
 
-        fishList.add(new Fish("고등어", 5000));
-        fishList.add(new Fish("광어", 25000));
-        fishList.add(new Fish("우럭", 27000));
-        fishList.add(new Fish("참돔", 30000));
-        fishList.add(new Fish("돌돔", 35000));
-        fishList.add(new Fish("능성어", 70000));
-        fishList.add(new Fish("다금바리", 120000));
-
+        ArrayList<Fish> fishList1 = new ArrayList<>();
+        Fish oneFishing1 = new Fish(); // 이걸 못해서 몇시간 멍때렸다 메서드 불러오기 선언을 해서 불러와야된다 스캐너처럼 ㅠㅠ
         Scanner scan = new Scanner(System.in);
 
-// 잡은 물고기를 담을 가방
-        ArrayList<Fish> fishBag = new ArrayList<>();
+        fishList1.add(new Fish("고등어", 5000));
+        fishList1.add(new Fish("광어", 25000));
+        fishList1.add(new Fish("우럭", 27000));
+        fishList1.add(new Fish("참돔", 30000));
+        fishList1.add(new Fish("돌돔", 35000));
+        fishList1.add(new Fish("능성어", 70000));
+        fishList1.add(new Fish("다금바리", 120000));
 
-        while(true) {
+
+
+//잡은 물고기를 담을 가방
+        ArrayList<Fish> fishBag1 = new ArrayList<>();
+
+        while (true) {
             System.out.println("행동을 선택해주세요.");
             System.out.println("1. 낚시하기 | 2. 잡은 물고기 목록 | 3. 떠나기");
             System.out.print(">>> ");
-            
 
             int select = Integer.parseInt(scan.nextLine());
+            int sum = 0;
+            // 낚시하기를 누르면 1번 낚시(0~7까지 랜덤으로 나온숫자에 해당하는 물고기)한 물고기 출력까지
+            if (select == 1) {
+                // 낚시한 물고기 리스트를 새로운 ArrayList에 담는다
+                fishList1 = oneFishing1.fishing2();
+                // 낚시해서 저장해놓은 모든 물고기 요소를 fishBag에 추가한다
+                for (int i = 0; i < fishList1.size(); i++) {
+                    fishBag1.add(fishList1.get(i));
+                }
 
-            if(select == 1) {
-                fishing();
                 // TODO 낚시하기
-            }else if(select == 2) {
+            } else if (select == 2) {
                 // TODO 잡은 물고기 목록 보기
-            }else if(select == 3) {
+                // fishBag에 담아놓은 모든 물고기를 출력한다
+                System.out.println("내 가방 ==================");
+                for (int i = 0; i < fishBag1.size(); i++) {
+
+                    System.out.println(fishBag1.get(i));
+                    // fishBag에 잡힌 물고기 값을 다 합친다
+                    sum += (fishBag1.get(i).getPrice());
+                }
+                System.out.println("총 " + sum + "원 " + "=================");
+            } else if (select == 3) {
                 // TODO 종료
+                System.out.println("가지마세요ㅠㅠ");
                 break;
             }
-            System.out.println();
-        }
 
+        }
     }
+
 }
